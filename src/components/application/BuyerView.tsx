18b876fb7c56409981c8b768fb4c305114df73ad
Abhinandan Jaisawal <abhinandan.jaisawal@webcraftit.com>
2021-07-18 19:16:21 +0530

    React Typescript Project with webpack

---
+++ src/components/application/BuyerView.tsx
@@ -0,0 +1,10 @@
+import "./buyersView.css";
+export const BuyerView = () => {
+  return (
+    <>
+      <h1>
+        Typescript React project template with webpack, eslint and prettier
+      </h1>
+    </>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import ReactDOM from "react-dom";
+import { BuyerView } from "./components/application/BuyerView";
+ReactDOM.render(<BuyerView />, document.getElementById("root"));
